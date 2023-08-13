ee2078a9d63990784e38f7445bb22ff006ca4a9a
Santiago Rivarola <srivarola@gmail.com>
2023-08-13 13:35:47 -0300

    fix products.js

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -1,44 +1,23 @@
-const nextButton = document.getElementById('right')
 function nextPage(hasNextPage, nextPage) {
-    if(!hasNextPage) {
-        nextButton.setAttribute("disabled");
-    } else {
-        nextButton.removeAttribute("disabled");
-    }
-    const location = window.location.href
-    const locationArray = location.split("/")
-    if(locationArray[locationArray.length - 1] == "products") {
-        const newLocation = location + "?page=" + nextPage
-        window.location.replace(newLocation);
-    } else {
-        const newLocation = window.location.protocol + "//localhost:8080/products/?page=" + nextPage;
-        window.location.replace(newLocation)
-    }
-
-    
+    if(hasNextPage) {
+        const location = window.location.href
+        const query = location.split('?')
+        const title = query[1] ? query[1].includes('title') : false
+        window.location.href = `/products/${title ? ('?',query[1],'&') : ''}?page=${nextPage}`
+    } 
 }
 
-const prevButton = document.getElementById('left');
 function prevPage(hasPrevPage, prevPage) {
-    if(!hasPrevPage) {
-        prevButton.setAttribute("disabled");
-    } else {
-        prevButton.removeAttribute("disabled");
-    }
-    const location = window.location.href
-    const locationArray = location.split("/")
-    if(locationArray[locationArray.length - 1] == "products") {
-        const newLocation = location + "?page=" + prevPage
-        window.location.replace(newLocation);
-    } else {
-        const newLocation = window.location.protocol + "//localhost:8080/products/?page=" + prevPage;
-        window.location.replace(newLocation)
+    if(hasPrevPage) {
+        const location = window.location.href
+        const query = location.split('?')
+        const title = query[1].includes('title')
+        window.location.href = `/products/${title ? ('?',query[1],'&') : ''}?page=${prevPage}` 
     }
 }
 
 const filterForm = document.getElementById('filter_form')
 filterForm.addEventListener('submit', handleFilterSubmit)
-
 
 function handleFilterSubmit(e) {
     e.preventDefault();
