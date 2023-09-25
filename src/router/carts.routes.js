4b0a26d82bd4712c02d4edaaed8a9dc51b9278ee
Gonzor <gonzalo.marodriguez@gmail.com>
2023-09-24 23:10:04 -0300

    carts /router/Variables/commander/ambientes

--- src/router/carts.routes.js
+++ src/router/carts.routes.js
@@ -1,59 +1,82 @@
-import { Router } from 'express';
+//import { Router } from 'express';
 //imports models for mongoose
+import MyRouter from './router.js';
+
 import Cart from '../dao/models/carts.js';
 import Product from '../dao/models/products.js';
-// imports manager for FS
-import CartsManager from '../dao/manager/CartsManager.js';
-import ProductManager from '../dao/manager/ProductManager.js';
-import __dirname from '../../utils.js';
+import passport from 'passport';
 
-const cartsRouter = Router();
 
-// manager FS
-// const manager = new CartsManager(`${__dirname}/files/carts.json`);
-// const productManager = new ProductManager(`${__dirname}/files/products.json`)
-
-//create
-cartsRouter.post('/', async (req, res, next) => {
-    try {
-        const cart = await Cart.create({Products: []})
-        return res.status(201).json({
-            success: true,
-            message: `Cart id: ${cart._id}`,
-            payload: cart
+export default class CartsTouter extends MyRouter {
+    init() {
+        this.post('/', ["ADMIN", "USER"], passport.authenticate("current"), async (req, res, next) => {
+            try {
+                let user = req.user;
+                let data = req.body;
+                data.user_id = user._id;
+                let response = await Cart.create( data )
+                    return res.sendSuccessCreate(response); 
+            } catch (error) {
+                next(error)
+            }
         })
-    } catch (error) {
-        next(error)
+        this.read('/', ["USER", "ADMIN"], passport.authenticate("current"), async (req, res, next) => {
+            try {
+                let user_id = req.user._id;
+                let state = "pending";
+                if (req.query.state) {
+                    state = req.query.state;
+                }
+                let response = await Cart.find({user_id, state}, "product_id user_id quantity state").populate('user_id', 'first_name last_name mail photo').populate('product_id', '-createdAt -updatedAt -__v');
+                if (response.length > 0) {
+                    return res.sendSuccess(response)
+                } else {
+                    return res.sendNotFound()
+                }
+            } catch (error) {
+                next(error);
+            }
+        })
+        this.put(
+            '/:id', ["USER", "ADMIN"],
+            passport.authenticate("current"),
+            async (req, res, next) => {
+                try {
+                    let cart_id = req.params.id;
+                    let data = req.body;
+                    let response = await Cart.findByIdAndUpdate(cart_id, data, { new: true });
+                    if (response) {
+                        return res.sendSuccess(response);
+                    } else {
+                        return res.sendNotFound();
+                    }
+                } catch (error) {
+                    next(error);
+                }
+            }
+        )
+
+        this.delete(
+            '/:id',
+            ["USER", "ADMIN"],
+            passport.authenticate("current"),
+            async (req, res, next) => {
+                try {
+                    let cart_id = req.params.id;
+                    let response = await Cart.findByIdAndDelete(cart_id).select("product_id user_id quantity state").populate("user_id", "first_name last_name mail photo").populate("product_id", "-createdAt -updatedAt -__v");
+                    if (response) {
+                        res.sendSuccess(response);
+                    } else {
+                        return res.sendNotFound();
+                    }
+                } catch (error) {
+                    next(error);
+                }
+            }
+        )
     }
-
-//    esto es para FS
-//    const cart = await manager.addNewCart()
-//    res.send({ status: 'success', payload: cart })
-});
-
-//read cart
-cartsRouter.get('/:cid', async (req, res, next) => {
-    try {
-        const { cid } = req.params;
-        const cart = await Cart.findById(cid).populate({path: "products.product", select: "title price thumbnail"});
-        cart.products.sort((a, b) => a.product.title.localeCompare(b.product.title))
-        return res.status(200).json({
-            success: true,
-            message: `Cart id: ${cart._id}`,
-            payload: cart.products
-        });
-    } catch (error) {
-        next(error)
-    }
-
-    // esto es para FS
-    // const cartId = Number(req.params.cid);
-    // const cart = await manager.getCart(cartId)
-    // if(!cart) {
-    //     return res.status(400).send({ error: `The cart with id: ${cartId}, doesn't exist!` })
-    // } 
-    // res.send({ status: 'success', payload: cart })
-});
+}
+/* 
 
 cartsRouter.get('/bills/:cid', async (req, res, next) => {
     try {
@@ -68,52 +91,6 @@ cartsRouter.get('/bills/:cid', async (req, res, next) => {
     } catch (error) {
         next(error)
     }
-})
-
-//update
-cartsRouter.put('/:cid/product/:pid', async (req, res, next) => {
-    try {
-        const { cid, pid } = req.params;
-        const product = await Product.findById(pid);
-        const cart = await Cart.findById(cid);
-        console.log(product)
-        let products = cart.products;
-        let isInCart = products.find(e => e.product == pid);
-        console.log(isInCart)
-        let newCart;
-        if(!isInCart){
-            products.push({product: product._id, quantity: 1});
-            await Cart.findByIdAndUpdate(cid, {products: products});
-            newCart = await Cart.findById(cid);
-        } else {
-            const filteredCart = products.filter(e => e.product != pid);
-            isInCart.quantity = isInCart.quantity + 1;
-            await Cart.findByIdAndUpdate(cid, {products: [...filteredCart, isInCart]});
-            newCart = await Cart.findById(cid);
-        }
-        return res.status(200).json({
-            success: true,
-            message: `Product ${pid} added!`,
-            payload: newCart
-        });
-    } catch (error) {
-        next(error);
-    }
-
-    // esto es para FS
-    // const cartId = Number(req.params.cid);
-    // const productId = Number(req.params.pid);
-    // // verifico si el producto con este id existe!
-    // const product = await productManager.getProductsById(productId)
-    // if(!product) {
-    //     return res.status(400).send({ error: `The product with id: ${productId}, doesn't exist!` })
-    // } 
-
-    // const cart = await manager.addProduct(cartId, productId)
-    // if(!cart) {
-    //     return res.status(400).send({ error: `Can't find cart whith id: ${cartId}` })
-    // } 
-    // res.send({ status: 'success', payload: cart })
 })
 
 // delete produtc
@@ -166,4 +143,4 @@ cartsRouter.delete('/:cid', async (req, res, next) => {
     }
 })
 
-export default cartsRouter
+ */
--- src/router/index.routes.js
+++ src/router/index.routes.js
@@ -6,14 +6,16 @@ import cartsRouter from "./carts.routes.js";
 import rtProductsRouter from "./realTimeProducts.routes.js";
 import ProductRouter from "./products.routes.js";
 
-const auth = new AuthRouter()
-const product = new ProductRouter()
+const auth = new AuthRouter();
+const product = new ProductRouter();
+const cart = new cartsRouter();
 
 export default class IndexRouter extends MyRouter {
     init() {
         this.read('/', (req, res) => res.status(200).send('products store api'));
         this.use('/auth', auth.getRouter());
         this.use('/products', product.getRouter());
+        this.use('/carts', cart.getRouter());
     }
 }
 
