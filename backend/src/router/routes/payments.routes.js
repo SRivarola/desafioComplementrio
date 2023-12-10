d474643fb451a4472c79dbb9134819869a453672
Santiago Rivarola <srivarola@gmail.com>
2023-12-10 19:43:06 -0300

    stripe intent

--- backend/src/router/routes/payments.routes.js
+++ backend/src/router/routes/payments.routes.js
@@ -1,7 +1,34 @@
 import MyRouter from "../router.js";
+import env from "../../config/env.js";
+import Stripe from "stripe";
 
 export default class PaymentsRouter extends MyRouter {
     init() {
+
+        this.post(
+            '/paument-intents',
+            ["USER", "PREMIUM"],
+            async (req, res, next) => {
+                try {
+                    const { id, amount } = req.body;
+
+                    const data = {
+                        amount,
+                        currency: 'usd'
+                    }
+                    const stripe = new Stripe(env.STRIPE_KEY);
+                    const intent = await stripe.paymentIntents.create(data);
+
+                    return res.sendSuccessCreate({
+                        message: "done",
+                        payload: intent
+                    })
+
+                } catch (error) {
+                    next(error)
+                }
+            }
+        )
 
     }
 }
