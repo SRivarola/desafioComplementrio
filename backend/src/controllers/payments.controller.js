53b9ba0218cba924b063078baf733a23f11c55bf
Santiago Rivarola <srivarola@gmail.com>
2023-12-14 21:05:56 -0300

    fix clg

--- backend/src/controllers/payments.controller.js
+++ backend/src/controllers/payments.controller.js
@@ -3,9 +3,7 @@ import createService from '../services/payments.service.js'
 export default async function (req, res, next) {
   try {
     const { amount } = req.params;
-    console.log(amount)
     const intent = await createService(amount, 'usd')
-    console.log(intent)
     return res.sendSuccessCreate({
       message: "done",
       payload: intent,
